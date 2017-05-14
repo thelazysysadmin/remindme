1725b883e4c09ec715cf7f518a405f767186b693
Miles <miles.tristan.warburton@googlemail.com>
2017-05-15 00:57:27 +0100

    tweeks

---
+++ main.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+while True:
+
+ pass
--- notifications.py
+++ notifications.py
@@ -1,6 +1,15 @@
 from PyQt5 import Qt
 import sys
-app = Qt.QApplication(sys.argv)
-systemtray_icon = Qt.QSystemTrayIcon(Qt.QIcon("df"))
-systemtray_icon.show()
-systemtray_icon.showMessage('Title', 'Content')
+
+class notifications:
+
+    def buildNotification(self):
+        app = Qt.QApplication(sys.argv)
+        systemtray_icon = Qt.QSystemTrayIcon(Qt.QIcon("df"))
+        systemtray_icon.show()
+        systemtray_icon.showMessage('Title', 'Content')
+
+
+nots = notifications()
+
+nots.buildNotification()
